9098f6f0cde057bb444dd47af4b3296a96f9d8fe
Reagan Barrington <reagan@barringtonfamily.net>
2020-07-22 12:38:49 -0400

    mvp

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,25 +2,29 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n, count=0):
 
-  if n <= 0:
-      count += 1
-      return count
+def eating_cookies(n):
+    return recursive_eating_cookies(n)
 
-  # 3 at a time
-  if n > 2:
-      count = eating_cookies(n-3, count)
-  
-  # 2 at a time
-  if n > 1:
-      count = eating_cookies(n-2, count)
+def recursive_eating_cookies(n, count=0):
 
-  # 1 at a time
-  if n > 0:
-      count = eating_cookies(n-1, count)
+    if n <= 0:
+        count += 1
+        return count
 
-  return count
+    # 3 at a time
+    if n > 2:
+        count = recursive_eating_cookies(n-3, count)
+    
+    # 2 at a time
+    if n > 1:
+        count = recursive_eating_cookies(n-2, count)
+
+    # 1 at a time
+    if n > 0:
+        count = recursive_eating_cookies(n-1, count)
+
+    return count
   
 
 if __name__ == "__main__":
